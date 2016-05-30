032be91d425a0764bbfa19222ee6a7d6b16d2e7b
Adam Breaux <breauxaj@gmail.com>
2016-05-30 16:52:50 -0400

    no longer needed

--- lib/facter/java_version.rb
+++
@@ -1,9 +0,0 @@
-require 'facter'
-Facter.add(:java_version) do
-  confine :osfamily => %w{RedHat}
-  setcode do
-    osr = Facter.value(:operatingsystemrelease)
-    output = osr[0,1]
-    output
-  end
-end
